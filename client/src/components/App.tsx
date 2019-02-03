e1a9bb532dcbc7fe4046a1fee772a6edc8ce1ad6
Ben Hall <benji2357@gmail.com>
2019-02-03 17:59:22 +0000

    feat(header): add

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -3,12 +3,15 @@ import Notes from './Notes'
 import SignIn from './SignIn'
 import SignUp from './SignUp'
 import VerifyUser from './VerifyUser'
+import { Header } from 'eri'
 
 export default function App() {
   return (
     <>
-      <main>
+      <Header>
         <h1>Webnotes</h1>
+      </Header>
+      <main>
         <h2>About</h2>
         <p>A web app for notes that's under construction.</p>
         <SignIn />
