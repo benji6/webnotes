a14547d55886685ba044b2a6e35a5ee98b2b0e50
Ben Hall <benji2357@gmail.com>
2019-02-26 06:56:03 +0000

    feat(api): save userId against notes

--- api/src/notesPost.js
+++ api/src/notesPost.js
@@ -2,18 +2,25 @@ const AWS = require('aws-sdk')
 
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
 
+const atob = a => Buffer.from(a, 'base64').toString('binary')
+
 exports.handler = (event, context, callback) => {
   let note
+  let userId
+
   try {
     note = JSON.parse(event.body)
+    userId = JSON.parse(atob(event.headers.Authorization.split('.')[1])).sub
   } catch (e) {
     return callback(e)
   }
+
   dynamodb.putItem(
     {
       Item: {
         body: { S: note.body },
         id: { S: String(Math.floor(Math.random() * 1e8)) },
+        userId: { S: userId },
       },
       TableName: 'webnotes_notes',
     },
