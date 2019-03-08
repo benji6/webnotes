06052bccb3d4a063b94d372e3a821211f703b637
Ben Hall <benji2357@gmail.com>
2019-03-08 19:18:45 +0000

    feat(EditNote): much better UX

--- client/src/components/pages/EditNote.tsx
+++ client/src/components/pages/EditNote.tsx
@@ -1,17 +1,38 @@
-import { Link } from '@reach/router'
+import { Link, NavigateFn } from '@reach/router'
 import * as React from 'react'
-import { TextArea, ButtonGroup, Button } from 'eri'
+import { TextArea, ButtonGroup, Button, Spinner } from 'eri'
 import { Form, Field } from 'react-final-form'
 import { putNote } from '../../api'
+import { NotesContext, SetNotesContext } from '../contexts'
 
 interface IProps {
   dateCreated: string
+  navigate: NavigateFn
 }
 
-export default function EditNote({ dateCreated }: IProps) {
-  return (
+export default function EditNote({ dateCreated, navigate }: IProps) {
+  const notes = React.useContext(NotesContext)
+  const setNotes = React.useContext(SetNotesContext)
+  const [isSubmitting, setIsSubmitting] = React.useState(false)
+  const note = (notes || []).find(note => note.dateCreated === dateCreated)
+
+  const handleSubmit = async ({ body }: any) => {
+    setIsSubmitting(true)
+    const newNote = await putNote({ body, dateCreated })
+    setNotes(notes => {
+      if (!notes) return [newNote]
+      const index = notes.findIndex(
+        ({ dateCreated }) => newNote.dateCreated === dateCreated,
+      )
+      return [newNote, ...notes.slice(0, index), ...notes.slice(index + 1)]
+    })
+    navigate('/')
+  }
+
+  return note ? (
     <Form
-      onSubmit={({ body }: any) => putNote({ body, dateCreated }) as any}
+      initialValues={{ body: note.body }}
+      onSubmit={handleSubmit}
       render={({ handleSubmit }) => (
         <form noValidate onSubmit={handleSubmit}>
           <h2>Edit note</h2>
@@ -20,11 +41,13 @@ export default function EditNote({ dateCreated }: IProps) {
             render={({ input }) => <TextArea {...input} label="Note" />}
           />
           <ButtonGroup>
-            <Button>Save</Button>
+            <Button disabled={isSubmitting}>Save</Button>
             <Link to="/">Cancel</Link>
           </ButtonGroup>
         </form>
       )}
     />
+  ) : (
+    <Spinner variant="page" />
   )
 }
