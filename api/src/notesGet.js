ebdbb4494314ae4bfa73f402412da3c6cbf536fe
Ben Hall <benji2357@gmail.com>
2019-03-30 09:06:29 +0000

    refactor(terraform): no longer use proxy integration for /notes GET

--- api/src/notesGet.js
+++ api/src/notesGet.js
@@ -2,17 +2,7 @@ const AWS = require('aws-sdk')
 
 const dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
 
-const atob = a => Buffer.from(a, 'base64').toString('binary')
-
-exports.handler = (event, context, callback) => {
-  let userId
-
-  try {
-    userId = JSON.parse(atob(event.headers.Authorization.split('.')[1])).sub
-  } catch (e) {
-    return callback(e)
-  }
-
+exports.handler = ({ userId }, context, callback) => {
   dynamodb.query(
     {
       ExpressionAttributeValues: { ':userId': { S: userId } },
@@ -21,26 +11,19 @@ exports.handler = (event, context, callback) => {
     },
     (error, data) => {
       if (error) return callback(error)
-      callback(null, {
-        body: JSON.stringify(
-          data.Items.map(({ body, dateCreated, dateUpdated, userId }) => ({
-            body: body.S,
-            dateCreated: dateCreated.S,
-            dateUpdated: dateUpdated.S,
-            userId: userId.S,
-          })).sort((a, b) => {
-            if (a.dateUpdated < b.dateUpdated) return 1
-            if (a.dateUpdated > b.dateUpdated) return -1
-            return 0
-          }),
-        ),
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Content-Type': 'application/json;charset=utf-8',
-          Vary: 'Origin',
-        },
-        statusCode: 200,
-      })
+      callback(
+        null,
+        data.Items.map(({ body, dateCreated, dateUpdated, userId }) => ({
+          body: body.S,
+          dateCreated: dateCreated.S,
+          dateUpdated: dateUpdated.S,
+          userId: userId.S,
+        })).sort((a, b) => {
+          if (a.dateUpdated < b.dateUpdated) return 1
+          if (a.dateUpdated > b.dateUpdated) return -1
+          return 0
+        }),
+      )
     },
   )
 }
--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -70,6 +70,32 @@ resource "aws_api_gateway_method" "notes_put" {
   rest_api_id   = "${aws_api_gateway_rest_api.api.id}"
 }
 
+resource "aws_api_gateway_method_response" "notes_get_200" {
+  depends_on  = ["aws_api_gateway_method.notes_get"]
+  http_method = "${aws_api_gateway_method.notes_get.http_method}"
+  resource_id = "${aws_api_gateway_resource.notes.id}"
+
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  status_code = 200
+}
+
+resource "aws_api_gateway_method_response" "notes_get_500" {
+  depends_on  = ["aws_api_gateway_method.notes_get"]
+  http_method = "${aws_api_gateway_method.notes_get.http_method}"
+  resource_id = "${aws_api_gateway_resource.notes.id}"
+
+  response_parameters {
+    "method.response.header.Access-Control-Allow-Origin" = true
+  }
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  status_code = 500
+}
+
 resource "aws_api_gateway_method_response" "notes_options_200" {
   depends_on  = ["aws_api_gateway_method.notes_options"]
   http_method = "${aws_api_gateway_method.notes_options.http_method}"
@@ -100,10 +126,19 @@ resource "aws_api_gateway_integration" "notes_delete" {
 resource "aws_api_gateway_integration" "notes_get" {
   http_method             = "${aws_api_gateway_method.notes_get.http_method}"
   integration_http_method = "POST"
-  resource_id             = "${aws_api_gateway_resource.notes.id}"
-  rest_api_id             = "${aws_api_gateway_rest_api.api.id}"
-  type                    = "AWS_PROXY"
-  uri                     = "${aws_lambda_function.notes_get.invoke_arn}"
+
+  request_templates = {
+    "application/json" = <<EOF
+{
+  "userId": "$context.authorizer.claims.sub"
+}
+EOF
+  }
+
+  resource_id = "${aws_api_gateway_resource.notes.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  type        = "AWS"
+  uri         = "${aws_lambda_function.notes_get.invoke_arn}"
 }
 
 resource "aws_api_gateway_integration" "notes_options" {
@@ -135,7 +170,47 @@ resource "aws_api_gateway_integration" "notes_put" {
   uri                     = "${aws_lambda_function.notes_put.invoke_arn}"
 }
 
+resource "aws_api_gateway_integration_response" "notes_get_200" {
+  depends_on = [
+    "aws_api_gateway_integration.notes_get",
+    "aws_api_gateway_method_response.notes_get_200",
+  ]
+
+  http_method = "${aws_api_gateway_method.notes_get.http_method}"
+  resource_id = "${aws_api_gateway_resource.notes.id}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  rest_api_id = "${aws_api_gateway_rest_api.api.id}"
+  status_code = "${aws_api_gateway_method_response.notes_get_200.status_code}"
+}
+
+resource "aws_api_gateway_integration_response" "notes_get_500" {
+  depends_on = [
+    "aws_api_gateway_integration.notes_get",
+    "aws_api_gateway_method_response.notes_get_500",
+  ]
+
+  http_method = "${aws_api_gateway_method.notes_get.http_method}"
+  resource_id = "${aws_api_gateway_resource.notes.id}"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Origin" = "'*'"
+  }
+
+  rest_api_id       = "${aws_api_gateway_rest_api.api.id}"
+  selection_pattern = "[^0-9](.|\n)*"
+  status_code       = "${aws_api_gateway_method_response.notes_get_500.status_code}"
+}
+
 resource "aws_api_gateway_integration_response" "notes_options" {
+  depends_on = [
+    "aws_api_gateway_integration.notes_options",
+    "aws_api_gateway_method_response.notes_options_200",
+  ]
+
   http_method = "${aws_api_gateway_method.notes_options.http_method}"
   resource_id = "${aws_api_gateway_resource.notes.id}"
   rest_api_id = "${aws_api_gateway_rest_api.api.id}"
@@ -146,11 +221,6 @@ resource "aws_api_gateway_integration_response" "notes_options" {
     "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS,GET,PUT,PATCH,DELETE'"
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
-
-  depends_on = [
-    "aws_api_gateway_integration.notes_options",
-    "aws_api_gateway_method_response.notes_options_200",
-  ]
 }
 
 resource "aws_api_gateway_deployment" "prod" {
