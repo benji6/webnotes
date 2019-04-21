248427d8b81b3ec3d0545011e3e5c3fd4bb5a7f3
Ben Hall <benji2357@gmail.com>
2019-04-21 22:48:31 +0100

    feat(icons): add basic icons for app

---
+++ client/scripts/icons.js
@@ -0,0 +1,46 @@
+const favicons = require('favicons')
+const fs = require('fs')
+const path = require('path')
+
+const sizes = ['144x144', '180x180', '192x192', '512x512']
+const iconsPath = path.join(__dirname, '..', 'src', 'icons')
+const source = path.join(iconsPath, 'icon.svg')
+
+const configuration = {
+  online: false,
+  preferOnline: false,
+  icons: {
+    android: true,
+    appleIcon: true,
+    appleStartup: true,
+    coast: false,
+    favicons: true,
+    firefox: false,
+    windows: false,
+    yandex: false,
+  },
+}
+
+favicons(source, configuration, (err, response) => {
+  if (err) throw err
+
+  response.images
+    .filter(({ name }) => {
+      if (
+        name.includes('favicon.ico') ||
+        name.includes('apple-touch-startup-image')
+      )
+        return true
+      for (const size of sizes) if (name.includes(size)) return true
+      return false
+    })
+    .map(({ contents, name }) => ({
+      contents,
+      name: name.replace('android-chrome', 'icon'),
+    }))
+    .forEach(({ contents, name }) =>
+      fs.writeFile(path.join(iconsPath, name), contents, err => {
+        if (err) throw err
+      }),
+    )
+})
