32a8d311d1c94691638b3be9b0844dfd7f004e59
Ben Hall <benji2357@gmail.com>
2019-05-05 12:44:21 +0100

    feat(About): update about page with links to the GitHub repo and issues

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -10,7 +10,26 @@ export default function About(_: RouteComponentProps) {
         notes. It's simple to use and because it runs in your browser you can
         use it across all your devices!
       </p>
-      <p>It is currently under construction.</p>
+      <p>
+        You can find the source code{' '}
+        <a
+          href="https://github.com/benji6/webnotes"
+          rel="noopener"
+          target="_blank"
+        >
+          here
+        </a>{' '}
+        and if you have any ideas, feedback or bugs you can raise them{' '}
+        <a
+          href="https://github.com/benji6/webnotes/issues"
+          rel="noopener"
+          target="_blank"
+        >
+          here
+        </a>
+        .
+      </p>
+      <p>I hope you enjoy the app :)</p>
     </>
   )
 }
