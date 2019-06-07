ad7beebc313d5fe4e3ee191ea9b687f0981a0c07
Ben Hall <benji2357@gmail.com>
2019-06-07 22:18:08 +0100

    feat(About): add emoji for smiley face

--- client/src/components/pages/About.tsx
+++ client/src/components/pages/About.tsx
@@ -29,7 +29,7 @@ export default function About(_: RouteComponentProps) {
         </a>
         .
       </p>
-      <p>I hope you enjoy the app :)</p>
+      <p>I hope you enjoy the app 🙂</p>
     </>
   )
 }
