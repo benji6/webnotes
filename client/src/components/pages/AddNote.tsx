984ad61dcb6dadb42a01f1d81100dd76531543c5
Ben Hall <benji2357@gmail.com>
2019-09-15 16:12:20 +0100

    refactor(AddNote): migrate from final-form to formik

--- client/src/components/pages/AddNote.tsx
+++ client/src/components/pages/AddNote.tsx
@@ -1,12 +1,15 @@
 import { RouteComponentProps, NavigateFn } from '@reach/router'
 import * as React from 'react'
 import { Fab, Icon, TextArea, PaperGroup, Paper, requiredValidator } from 'eri'
-import { Form, Field, FieldRenderProps } from 'react-final-form'
+import { Formik, Form, Field, FieldProps } from 'formik'
 import useRedirectUnAuthed from '../hooks/useRedirectUnAuthed'
 import useNotePlaceholder from '../hooks/useNotePlaceholder'
 import { useNotes } from '../containers/Notes'
 import { INoteLocal } from '../../types'
-import { errorProp } from '../../utils'
+
+interface IFormValues {
+  body: string
+}
 
 const bodyFieldName = 'body'
 
@@ -15,7 +18,7 @@ export default function AddNote({ navigate }: RouteComponentProps) {
   const [, setNotes] = useNotes()
   const placeholder = useNotePlaceholder()
 
-  const handleSubmit = async ({ body }: { body: string }) => {
+  const handleSubmit = async ({ body }: IFormValues) => {
     const dateCreated = new Date().toISOString()
     const note: INoteLocal = {
       body: body.trim(),
@@ -31,40 +34,34 @@ export default function AddNote({ navigate }: RouteComponentProps) {
     <PaperGroup>
       <Paper>
         <h2>Add note</h2>
-        <Form
-          onSubmit={handleSubmit as any}
-          render={({ handleSubmit }) => (
-            <form noValidate onSubmit={handleSubmit}>
-              <Field
-                name={bodyFieldName}
-                validate={requiredValidator}
-                render={({
-                  input,
-                  meta,
-                }: FieldRenderProps<string, HTMLElement>) => (
-                  <TextArea
-                    {...input}
-                    error={errorProp(meta)}
-                    label="Note"
-                    placeholder={placeholder}
-                    rows={14}
-                  />
-                )}
-              />
-              <Field name={bodyFieldName} subscription={{ value: true }}>
-                {({ input: { value } }) => (
-                  <Fab
-                    aria-label="save"
-                    hide={!(value && value.trim())}
-                    onClick={handleSubmit as any}
-                  >
-                    <Icon name="save" size="4" />
-                  </Fab>
-                )}
-              </Field>
-            </form>
-          )}
-        />
+        <Formik initialValues={{ body: '' }} onSubmit={handleSubmit}>
+          <Form noValidate>
+            <Field name={bodyFieldName} validate={requiredValidator}>
+              {({ field, form }: FieldProps<IFormValues>) => (
+                <TextArea
+                  {...field}
+                  error={
+                    form.submitCount && form.touched.body && form.errors.body
+                  }
+                  label="Note"
+                  placeholder={placeholder}
+                  rows={14}
+                />
+              )}
+            </Field>
+            <Field name={bodyFieldName}>
+              {({ field: { value } }: FieldProps<IFormValues>) => (
+                <Fab
+                  aria-label="save"
+                  hide={!value.trim()}
+                  onClick={() => handleSubmit({ body: value })}
+                >
+                  <Icon name="save" size="4" />
+                </Fab>
+              )}
+            </Field>
+          </Form>
+        </Formik>
       </Paper>
     </PaperGroup>
   )
