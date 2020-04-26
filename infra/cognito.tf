9c74e9c85e1b41e46a17456c367bdd7acbe3e7bd
Ben Hall <benji2357@gmail.com>
2020-04-26 18:16:24 +0100

    chore(infra): remove unused Cognito identity pool

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -24,16 +24,6 @@ resource "aws_cognito_user_pool_client" "main" {
   user_pool_id                  = aws_cognito_user_pool.main.id
 }
 
-resource "aws_cognito_identity_pool" "main" {
-  identity_pool_name               = "webnotes"
-  allow_unauthenticated_identities = false
-
-  cognito_identity_providers {
-    client_id     = aws_cognito_user_pool_client.main.id
-    provider_name = aws_cognito_user_pool.main.endpoint
-  }
-}
-
 resource "aws_cognito_user_pool_domain" "main" {
   domain       = "webnotes"
   user_pool_id = aws_cognito_user_pool.main.id
@@ -46,4 +36,3 @@ output "cognito_user_pool_id" {
 output "cognito_app_client_id" {
   value = aws_cognito_user_pool_client.main.id
 }
-
