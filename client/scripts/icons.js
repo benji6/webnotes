a4a870cad52ffff0065658dc5993588b7e3c3b3e
Ben Hall <benji2357@gmail.com>
2020-06-28 22:13:35 +0100

    chore(format): expand use of prettier

--- client/scripts/icons.js
+++ client/scripts/icons.js
@@ -1,9 +1,9 @@
-const favicons = require('favicons')
-const fs = require('fs')
-const path = require('path')
+const favicons = require("favicons");
+const fs = require("fs");
+const path = require("path");
 
-const iconsPath = path.join(__dirname, '..', 'src', 'icons')
-const source = path.join(iconsPath, 'icon.svg')
+const iconsPath = path.join(__dirname, "..", "src", "icons");
+const source = path.join(iconsPath, "icon.svg");
 
 const configuration = {
   online: false,
@@ -18,27 +18,27 @@ const configuration = {
     windows: false,
     yandex: false,
   },
-}
+};
 
 favicons(source, configuration, (err, response) => {
-  if (err) throw err
+  if (err) throw err;
 
   response.images
     .filter(({ name }) =>
       [
-        'android-chrome-192x192.png',
-        'android-chrome-512x512.png',
-        'apple-touch-icon.png',
-        'favicon.ico',
-      ].includes(name),
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "apple-touch-icon.png",
+        "favicon.ico",
+      ].includes(name)
     )
     .map(({ contents, name }) => ({
       contents,
-      name: name.replace('android-chrome', 'icon'),
+      name: name.replace("android-chrome", "icon"),
     }))
     .forEach(({ contents, name }) =>
-      fs.writeFile(path.join(iconsPath, name), contents, err => {
-        if (err) throw err
-      }),
-    )
-})
+      fs.writeFile(path.join(iconsPath, name), contents, (err) => {
+        if (err) throw err;
+      })
+    );
+});
--- client/scripts/injectCacheList.js
+++ client/scripts/injectCacheList.js
@@ -1,17 +1,17 @@
-const path = require('path')
-const fs = require('fs').promises
+const path = require("path");
+const fs = require("fs").promises;
 
-const blacklist = ['index.html', 'robots.txt', 'service-worker.js']
-const buildPath = path.join(__dirname, '..', 'dist')
+const blacklist = ["index.html", "robots.txt", "service-worker.js"];
+const buildPath = path.join(__dirname, "..", "dist");
 
-;(async () => {
-  const files = await fs.readdir(buildPath)
-  const filteredFiles = files.filter(file => !blacklist.includes(file))
-  const cacheList = ['/', ...filteredFiles]
-  const serviceWorkerPath = path.join(buildPath, 'service-worker.js')
-  const serviceWorker = await fs.readFile(serviceWorkerPath, 'utf-8')
+(async () => {
+  const files = await fs.readdir(buildPath);
+  const filteredFiles = files.filter((file) => !blacklist.includes(file));
+  const cacheList = ["/", ...filteredFiles];
+  const serviceWorkerPath = path.join(buildPath, "service-worker.js");
+  const serviceWorker = await fs.readFile(serviceWorkerPath, "utf-8");
   return fs.writeFile(
     serviceWorkerPath,
-    serviceWorker.replace('CACHE_LIST', cacheList),
-  )
-})()
+    serviceWorker.replace("CACHE_LIST", cacheList)
+  );
+})();
