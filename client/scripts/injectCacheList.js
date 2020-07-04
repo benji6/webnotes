c7c90a7d75290236b8efeca216bec8a40f35b50b
Ben Hall <benji2357@gmail.com>
2020-07-04 12:43:54 +0100

    refactor(scripts): change blacklist to ignoreSet

--- client/scripts/injectCacheList.js
+++ client/scripts/injectCacheList.js
@@ -1,12 +1,12 @@
 const path = require("path");
 const fs = require("fs").promises;
 
-const blacklist = ["index.html", "robots.txt", "service-worker.js"];
+const ignoreSet = new Set(["index.html", "robots.txt", "service-worker.js"]);
 const buildPath = path.join(__dirname, "..", "dist");
 
 (async () => {
   const files = await fs.readdir(buildPath);
-  const filteredFiles = files.filter((file) => !blacklist.includes(file));
+  const filteredFiles = files.filter((file) => !ignoreSet.has(file));
   const cacheList = ["/", ...filteredFiles];
   const serviceWorkerPath = path.join(buildPath, "service-worker.js");
   const serviceWorker = await fs.readFile(serviceWorkerPath, "utf-8");
