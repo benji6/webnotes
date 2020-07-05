27fba70b815cb81b36663a2688d10238866b3419
Ben Hall <benji2357@gmail.com>
2020-07-05 18:12:53 +0100

    feat(SeeAlso): update with MoodTracker

--- client/src/components/pages/SeeAlso.tsx
+++ client/src/components/pages/SeeAlso.tsx
@@ -7,18 +7,29 @@ export default function SeeAlso(_: RouteComponentProps) {
     <Paper.Group>
       <Paper>
         <h2>See also</h2>
-        <p>
-          <a
-            href="https://meditation-timer.link"
-            rel="noopener"
-            target="_blank"
-          >
-            Meditate
-          </a>{" "}
-          is a free and open source web app that allows you to time your
-          meditations. It's simple to use, works offline and aims to be the
-          perfect aide for your meditation practice!
-        </p>
+        <p>Here are some other things I've made that you might like:</p>
+        <ul>
+          <li>
+            <a
+              href="https://meditation-timer.link"
+              rel="noopener"
+              target="_blank"
+            >
+              Meditate
+            </a>{" "}
+            is a free and open source web app that allows you to time your
+            meditations. It's simple to use, works offline and aims to be the
+            perfect aide for your meditation practice!
+          </li>
+          <li>
+            <a href="https://moodtracker.link" rel="noopener" target="_blank">
+              MoodTracker
+            </a>{" "}
+            is a free and open source web app that lets you track your mood.
+            It's simple to use, works offline and because it runs in your
+            browser you can use it across all your devices!
+          </li>
+        </ul>
       </Paper>
     </Paper.Group>
   );
