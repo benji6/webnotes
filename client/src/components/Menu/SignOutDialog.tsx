b42317f66ee496fc5aec0fe56bb9d12e76ee0c36
Ben Hall <benji2357@gmail.com>
2020-08-31 20:40:09 +0100

    feat(SignOut): add info to user on whether sign out will cause data loss

--- client/src/components/Menu/SignOutDialog.tsx
+++ client/src/components/Menu/SignOutDialog.tsx
@@ -2,7 +2,7 @@ import { navigate } from "@reach/router";
 import { Dialog, Button } from "eri";
 import * as React from "react";
 import { userPool } from "../../cognito";
-import { DispatchContext } from "../AppState";
+import { DispatchContext, StateContext } from "../AppState";
 
 interface Props {
   onClose(): void;
@@ -10,6 +10,7 @@ interface Props {
 }
 
 export default function SignOutDialog({ onClose, open }: Props) {
+  const state = React.useContext(StateContext);
   const dispatch = React.useContext(DispatchContext);
   const [isLoading, setIsLoading] = React.useState(false);
 
@@ -31,6 +32,17 @@ export default function SignOutDialog({ onClose, open }: Props) {
       open={open}
       title="Sign out?"
     >
+      {state.notes && state.notes.some(({ syncState }) => syncState) ? (
+        <p>
+          <strong>
+            WARNING: some of your data has not yet been synced to the server and
+            will be lost if you sign out now. If you don't want to lose any data
+            please connect to the internet to sync before logging out.
+          </strong>
+        </p>
+      ) : (
+        <p>Safe to sign out, all data is synced to the server.</p>
+      )}
       <Button.Group>
         <Button danger disabled={isLoading} onClick={handleSignOut}>
           Sign out
