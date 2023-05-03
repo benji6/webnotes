36a5fc6201cd67020fb110e8769107bb01badfe6
Ben Hall <benji2357@gmail.com>
2023-05-03 12:47:43 +0100

    chore(api): format code

--- api/src/notes_patch.py
+++ api/src/notes_patch.py
@@ -1,25 +1,27 @@
-import boto3
 import json
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('webnotes_notes')
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("webnotes_notes")
+
 
 def handler(event, context):
-  user_id = event['requestContext']['authorizer']['claims']['sub']
-  try:
-    patch = json.loads(event['body'])
-    with table.batch_writer() as batch:
-      if 'delete' in patch:
-        for id in patch['delete']:
-          batch.delete_item(Key={'dateCreated': id, 'userId': user_id})
-      if 'put' in patch:
-        for note in patch['put']:
-          note['userId'] = user_id
-          batch.put_item(Item=note)
-    return { 'statusCode': 204 }
-  except Exception as e:
-    print(e)
-    return {
-      'body': 'Internal server error',
-      'statusCode': 500,
-    }
+    user_id = event["requestContext"]["authorizer"]["claims"]["sub"]
+    try:
+        patch = json.loads(event["body"])
+        with table.batch_writer() as batch:
+            if "delete" in patch:
+                for id in patch["delete"]:
+                    batch.delete_item(Key={"dateCreated": id, "userId": user_id})
+            if "put" in patch:
+                for note in patch["put"]:
+                    note["userId"] = user_id
+                    batch.put_item(Item=note)
+        return {"statusCode": 204}
+    except Exception as e:
+        print(e)
+        return {
+            "body": "Internal server error",
+            "statusCode": 500,
+        }
