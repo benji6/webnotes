49689ec0526257d0f226af04171360a44059189f
Ben Hall <benji2357@gmail.com>
2023-06-15 06:18:43 +0300

    fix(useNotePlaceholder): fix issue where the placeholder changes unexpectedly

--- client/src/components/hooks/useNotePlaceholder.ts
+++ client/src/components/hooks/useNotePlaceholder.ts
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 const shuffle = <A>(xs: A[]) => {
   for (let i = xs.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -7,17 +9,21 @@ const shuffle = <A>(xs: A[]) => {
 };
 
 const placeholders = shuffle([
-  "Add a new note here",
-  "Note to self...",
-  "What are you up to?",
-  "Need to remember something?",
-  "Write your note here",
+  "➕ Add a new note here",
+  "📝 Note to self...",
+  "❓ What are you up to?",
+  "🤔 Need to remember something?",
+  "✏️ Write your note here",
+  "✅ something you need to do?",
 ]);
 
 let notePlaceholderIndex = -1;
 
 export default function useNotePlaceholder() {
-  if (notePlaceholderIndex === placeholders.length - 1)
-    notePlaceholderIndex = 0;
-  return placeholders[++notePlaceholderIndex];
+  useEffect(() => {
+    if (++notePlaceholderIndex > placeholders.length - 1)
+      notePlaceholderIndex = 0;
+  }, []);
+
+  return placeholders[notePlaceholderIndex];
 }
