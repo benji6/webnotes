99550dd5d01640b7707e2e885efae2dc0a5d71c0
Ben Hall <benji2357@gmail.com>
2024-02-18 21:52:08 +0000

    perf(service-worker): split cache by network first and cache first

---
+++ client/scripts/injectCacheLists.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const ignoreSet = new Set(["index.html", "robots.txt", "service-worker.js"]);
+const buildPath = path.join(__dirname, "..", "dist");
+
+(async () => {
+  const files = await fs.readdir(buildPath);
+  const filteredFiles = files.filter(
+    (file) => !(ignoreSet.has(file) || file.endsWith(".map")),
+  );
+  const networkFirstCache = ["/"];
+  const cacheFirstCache = [];
+  for (const file of filteredFiles) {
+    if (/^.+\..{8}\..{2,5}$/.test(file)) cacheFirstCache.push(file);
+    else networkFirstCache.push(file);
+  }
+
+  // These checks are intentionally paranoid
+  if (
+    cacheFirstCache.some((resource) =>
+      ["icon-without-css", "robots", "service-worker", "webmanifest"].some(
+        (word) => resource.includes(word),
+      ),
+    ) ||
+    cacheFirstCache.includes("/") ||
+    cacheFirstCache.some((resource) => networkFirstCache.includes(resource))
+  )
+    throw Error("Check cache lists");
+
+  const serviceWorkerPath = path.join(buildPath, "service-worker.js");
+  const serviceWorker = await fs.readFile(serviceWorkerPath, "utf-8");
+  return fs.writeFile(
+    serviceWorkerPath,
+    serviceWorker
+      .replace("CACHE_FIRST_CACHE", cacheFirstCache)
+      .replace("NETWORK_FIRST_CACHE", networkFirstCache),
+  );
+})();
