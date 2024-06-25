f0804d7742ea36169f5f1f021863e7fe941421fa
Ben Hall <benji2357@gmail.com>
2024-06-25 21:43:00 +0300

    chore(dynamo): add deletion protection

--- infra/dynamodb.tf
+++ infra/dynamodb.tf
@@ -1,8 +1,9 @@
 resource "aws_dynamodb_table" "notes" {
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "userId"
-  name         = "webnotes_notes"
-  range_key    = "dateCreated"
+  billing_mode                = "PAY_PER_REQUEST"
+  deletion_protection_enabled = true
+  hash_key                    = "userId"
+  name                        = "webnotes_notes"
+  range_key                   = "dateCreated"
 
   attribute {
     name = "dateCreated"
